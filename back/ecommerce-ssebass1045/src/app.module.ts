0b5fe45c9a1c074e2d49f727a85747ef336bef79
ssebass1045 <sebas.22.26@hotmail.com>
2024-07-16 18:33:57 -0500

    modules and endpoints exit

--- back/ecommerce-ssebass1045/src/app.module.ts
+++ back/ecommerce-ssebass1045/src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductsModule } from './Products/Products.module';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [ProductsModule, UsersModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
