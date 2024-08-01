cee1abbd422708924b98d4d9244df725650ada59
ssebass1045 <sebas.22.26@hotmail.com>
2024-08-01 03:51:46 -0500

    Auth 2 aplicated with de PUT product with guard

--- back/ecommerce-ssebass1045/src/Products/Products.controller.ts
+++ back/ecommerce-ssebass1045/src/Products/Products.controller.ts
@@ -2,6 +2,9 @@ import { Controller, Body, Delete, Get, Param, Query, HttpCode, Post, Put, UseGu
 import { ProductsService } from "./products.service";
 import { CreateProductDto } from "./dto/cretae-product.dto";
 import { AuthGuard } from "src/auth/AuthGuard";
+import { RolesGuard } from "src/auth/roles.guard";
+import { Roles } from "src/decorators/roles.decorator";
+import { Role } from "src/roles.enum";
 
 @Controller('products')
 export class ProductsController {
@@ -27,7 +30,8 @@ export class ProductsController {
     }
     
     @Put(":id")
-    @UseGuards(AuthGuard)
+    @Roles(Role.Admin)
+    @UseGuards(AuthGuard, RolesGuard)
     updateProduct(@Query('id') id: string, @Body() product:any ){
       return this.productsService.update(id, product);
     }
